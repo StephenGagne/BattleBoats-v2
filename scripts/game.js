ebf41d1d7239a398d2f70f2cc47bccdf0718ebb8
Stephen Gagne <gagn0474@algonquinlive.com>
2018-11-09 10:20:37 -0500

    Basic Functionality

---
+++ scripts/game.js
@@ -0,0 +1,82 @@
+const $boatContainer = document.getElementById('boats')
+const boats = $boatContainer.getElementsByClassName('boat')
+const $board = document.getElementById('gameboard')
+const $rows = $board.getElementsByClassName('row')
+const $cols = Array.from($board.getElementsByClassName('col'))
+let boatLength = 0
+let boatColor = null
+let orientation = 'horizontal'
+
+$boatContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('boat')) {
+    const thisBoat = e.target.closest('.boat')
+    boatColor = window.getComputedStyle(thisBoat).backgroundColor
+    for (const boat of boats) {
+      boat.classList.remove('active')
+    }
+    e.target.classList.add('active')
+    boatLength = e.target.dataset.length
+  }
+})
+
+$board.addEventListener('mouseover', function (cell) {
+  for (const col of $cols) {
+    if (!col.classList.contains('locked')) {
+      col.style.backgroundColor = "#fff"
+      col.classList.remove('ship')
+    }
+  }
+  if (cell.target.classList.contains('col') && !cell.target.classList.contains('locked')) {
+    if (orientation === 'horizontal') {
+      let row = Array.from(cell.target.closest('.row').querySelectorAll('.col'))
+      let startIndex = row.indexOf(cell.target)
+      if (startIndex > row.length - boatLength) {
+        startIndex = row.length - boatLength
+      }
+      for (let i = 0; i < boatLength; i++) {
+        if (!row[startIndex + i].classList.contains('locked')) {
+          row[startIndex + i].style.backgroundColor = boatColor
+          row[startIndex + i].classList.add('ship')
+        }
+      }
+    } else {
+      let row = cell.target.closest('.row')
+      let rowArray = Array.from(cell.target.closest('.row').querySelectorAll('.col'))
+      let allRows = Array.from($rows)
+      let rowIndex = allRows.indexOf(row)
+      let startIndex = rowArray.indexOf(cell.target)
+      // console.log($rows[allRows.indexOf(row)].querySelectorAll('.col')[startIndex])
+      if (rowIndex >= 10 - boatLength) {
+        rowIndex = 10 - boatLength
+      }
+      for (let i = 0; i < boatLength; i++) {
+        if ($rows[rowIndex + i].querySelectorAll('.col')[startIndex].classList.contains('locked')) {
+          break
+        }
+        let ship = $rows[rowIndex + i].querySelectorAll('.col')[startIndex]
+        ship.style.backgroundColor = boatColor
+        ship.classList.add('ship')
+      }
+    }
+  }
+})
+
+$board.addEventListener('click', function (e) {
+  for (const col of $cols) {
+    if (col.classList.contains('ship')) {
+      col.classList.add('locked')
+    }
+  }
+  boatColor = null
+  boatLength = 0
+})
+
+window.addEventListener('keypress', function (e) {
+  if (e.key === 'r') {
+    if (orientation === 'horizontal') {
+      orientation = 'vertical'
+    } else {
+      orientation = 'horizontal'
+    }
+  }
+})
